3df298cd4390724fdcf9232aeb07b5516041fd5d
Bowen Zhang <bowenzhang@Bowens-MBP.fios-router.home>
2020-05-24 19:56:44 -0400

    updated bio and added wip link to covid tracker

--- src/mock/data.js
+++ src/mock/data.js
@@ -18,9 +18,9 @@ export const heroData = {
 // ABOUT DATA
 export const aboutData = {
   img: 'profile.jpg',
-  paragraphOne: `I'm a 2017 Cornell grad & former consultant currently working at Yext in the UPWARD rotational program.`,
-  paragraphTwo: `I recently discovered the beauty of programming and am glad you've taken the time to take a look at my personal projects!`,
-  paragraphThree: `Outside of work I also enjoy poker, electronic music, Super Smash Bros. and getting better at picking heavy things up and putting them down`,
+  paragraphOne: `I'm a 2017 Cornell grad with a B.S. in Finance & Strategy. As a former management consultant with ZS Associates, I decided to pursue my passion for technology and am currently working at Yext in the UPWARD rotational program.`,
+  paragraphTwo: `I'm a self-taught full stack developer with experience in Java, Python, and Javascript.`,
+  paragraphThree: `Outside of programming, I also enjoy poker, electronic music, Super Smash Bros. and getting better at picking heavy things up and putting them down`,
   resume: 'https://www.linkedin.com/in/bowenzhangdotcom/',
 };
 
@@ -35,15 +35,25 @@ export const projectsData = [
   //   url: '',
   //   repo: 'https://github.com/cobidev/react-simplefolio',
   // },
+  // {
+  //   id: uuidv1(),
+  //   img: 'nice-stocks-logo.jpg',
+  //   title: 'NiceStocks.Info - WIP',
+  //   info:
+  //     'Tracks comparisons of stock price changes to discussions on investing-related subreddits',
+  //   info2: 'Currently building with React',
+  //   url: 'www.nicestocks.info',
+  //   repo: 'https://github.com/bowenzhangdotcom',
+  // },
   {
     id: uuidv1(),
-    img: 'nice-stocks-logo.jpg',
-    title: 'NiceStocks.Info - WIP',
+    img: 'covid-placeholder.png',
+    title: 'COVID-19 Tracker - WIP',
     info:
-      'Tracks comparisons of stock price changes to discussions on investing-related subreddits',
-    info2: 'Currently building with React',
-    url: 'www.nicestocks.info',
-    repo: 'https://github.com/bowenzhangdotcom',
+      'Building a global tracker for COVID-19 infections, recoveries, and deaths. Data is filterable by country and is updated in real-time.',
+    info2: 'Using React, Chart.js, Material UI, and data from the Johns Hopkins CSSE API',
+    url: '#',
+    repo: 'https://github.com/bowenzhangdotcom/corona-tracker',
   },
 ];
 
